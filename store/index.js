a585e15dc4085fa2c8bc7cf7f9db398724713abe
zjb19970804 <1070179206@qq.com>
2019-04-16 13:27:36 +0800

    fixNODE_ENV

--- store/index.js
+++ store/index.js
@@ -1,14 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 const cookieparser = process.server ? require('cookieparser') : undefined
-
 Vue.use(Vuex)
 export default () =>
   new Vuex.Store({
     state: {
       token: null,
       clientId: 'b17fdfd4cd7c3ccf21de',
-      imageUrl: process.env === 'production' ? 'http://45.77.86.164:7001' : 'http://127.0.0.1:7001'
+      imageUrl: process.env.NODE_ENV === 'production' ? 'http://45.77.86.164:7001' : 'http://127.0.0.1:7001'
     },
     mutations: {
       setToken(state, value = null) {
